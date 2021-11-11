aa6ed7ad8f27ee0dc61165b2e911f1909b6ae267
“shafikulislam067@gmail.comgit config --global user.name “shafikul islam <shafikulislam067@gmail.comgit config --global user.email shafikulislam067@gmail.com>
2021-11-11 22:54:16 +0600

    add layout

---
+++ components/Layout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import style from '../styles/Layout.module.css'
+const Layout = () => {
+    return (
+        <div>
+            <h1 className={style.container}>Layout Next.js</h1>            
+        </div>
+    );
+};
+
+export default Layout;
---
+++ pages/about.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Head from 'next/head';
+
+const about = () => {
+    return (
+        <div>
+            <Head>
+                <title>About</title>
+                <meta name="description" content="abut next.js , all about next.js"/>
+            </Head>
+            <h1>Next about</h1>
+            
+        </div>
+    );
+};
+
+export default about;
