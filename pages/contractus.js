65e00112d27f619d442128fe40f932939d39a35f
“shafikulislam067@gmail.comgit config --global user.name “shafikul islam <shafikulislam067@gmail.comgit config --global user.email shafikulislam067@gmail.com>
2021-11-14 12:57:04 +0600

    contract Us

---
+++ pages/contractus.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const contractus = () => {
+    return (
+        <div>
+            <h1>Contract Us Now</h1>
+            
+        </div>
+    );
+};
+
+export default contractus;
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,7 @@
 import Head from 'next/head'
-import Layout from '../components/Layout'
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>Next.js Blog</title>
-        <meta name="keywords" content="Next.js, Blog Next.js"/>
-      </Head>
-
-      <div>
-        <h1>Welcome To Next.js Blog Web-site</h1>
-        <Layout/>
-      </div>
       
     </div>
   )
